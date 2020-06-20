d81865f1e1d6ec6210cf6fa0cec8ac8645e26eb8
EvaPanda666 <mega.vikulya@gmail.com>
2020-06-21 00:22:34 +0700

    код для пункта 2.4 курса

---
+++ les_2_5.py
@@ -0,0 +1,42 @@
+﻿from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    button2 = browser.find_element_by_id("book")
+    button2.click()
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_id("input_value")
+    x=input1.text
+    #print('x=',x)
+    y = calc(x)
+    #print(y)
+    input2 = browser.find_element_by_id("answer")
+    input2.send_keys(y)
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
+# не забываем оставить пустую строку в конце файла
